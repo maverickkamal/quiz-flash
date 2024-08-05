79c4a9a2afa00c908ba764430d2cee38986e2f8e
Kamaludeen Musa <150486956+maverickkamal@users.noreply.github.com>
2024-08-05 01:23:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from routes.firebase_utils import initialize_firebase
 from routes import flashcards, decks, study_sessions
-from quiz_routes import quizzes, algorithm
+# from quiz_routes import quizzes, algorithm
 
 
 
 app = FastAPI()
 
 
-app.include_router(algorithm.router)
-app.include_router(quizzes.router)
+# app.include_router(algorithm.router)
+# app.include_router(quizzes.router)
 app.include_router(flashcards.router)
 app.include_router(decks.router)
 app.include_router(study_sessions.router)
